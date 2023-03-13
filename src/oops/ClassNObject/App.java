e666ae95cddcc7111f5389cdbf217a8763cf20c4
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-03-14 01:19:52 +0530

    App

---
+++ src/oops/ClassNObject/App.java
@@ -0,0 +1,10 @@
+package oops.ClassNObject;
+
+public class App {
+    public static void main(String[] args) {
+
+        Student s = new Student();
+        s.print();
+        s.main();
+    }
+}
