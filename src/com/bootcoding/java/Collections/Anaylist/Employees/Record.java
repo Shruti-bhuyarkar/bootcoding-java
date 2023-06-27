64112d4d8354c5e8435e7f541e0eab86dc9884df
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-06-27 15:16:27 +0530

    record

---
+++ src/com/bootcoding/java/Collections/Anaylist/Employees/Record.java
@@ -0,0 +1,40 @@
+package com.bootcoding.java.Collections.Anaylist.Employees;
+
+public class Record {
+    public  String Name;
+    public double Experience;
+    public double Salary;
+    public int age;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public double getExperience() {
+        return Experience;
+    }
+
+    public void setExperience(double experience) {
+        Experience = experience;
+    }
+
+    public double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(double salary) {
+        Salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
